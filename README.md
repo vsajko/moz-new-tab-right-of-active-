df4b772494a9548c8f19c66c7e9dd4f31074ed36
Vedran Sajko <vsajko@reversinglabs.com>
2015-09-27 03:06:47 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+#New Tab right of active
+Simple Firefox extension that changes behaviour of opening new tab.
+Instead of far right tab is opened immediately right of active tab.
+There are already several extensions that do this.
---
+++ index.js
@@ -0,0 +1,29 @@
+var buttons = require('sdk/ui/button/action');
+var tabs = require("sdk/tabs");
+
+buttons.ActionButton({
+  id: "mozilla-link",
+  label: "Visit Mozilla",
+  icon: {
+    "16": "./icon-16.png",
+    "32": "./icon-32.png",
+    "64": "./icon-64.png"
+  },
+  onClick: handleClick
+});
+
+function handleClick() {
+    var oldIndex = tabs.activeTab.index;
+    tabs.open({
+        url: "about:newTab",
+        isPinned: false,
+        onOpen: function onOpen(tab) {
+            tab.index = oldIndex + 1;
+        }
+    });
+}
+var { Hotkey } = require("sdk/hotkeys");
+Hotkey({
+  combo: "accel-t",
+  onPress: handleClick
+});
